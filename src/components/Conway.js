5483fb667068cae066c37779f193e976ddaa2c6c
Sameer Kolhar <kolhar730@gmail.com>
2020-01-12 10:03:48 +0530

    Add prettier, fps config to app
    Now you can configure the fps on double click of screen.

--- src/components/Conway.js
+++ src/components/Conway.js
@@ -8,244 +8,271 @@ import { STARTERS } from "./constants/starters.constants";
 import "../styles/styles.css";
 
 class Conway extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      grid: this._getNewGrid(),
-      globalAnimationID: undefined,
-      isAnimating: false
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			grid: this._getNewGrid(),
+			globalAnimationID: undefined,
+			isAnimating: false,
+		};
+		this.timeInterval = 1000 / this.props.fps;
+		this.previousFrame = window.performance.now();
+	}
 
-  _getNewGrid = (preset = []) => {
-    const presetProvided = preset.length > 0;
-    const { rows, cols } = this.props;
-    let grid = [];
-    for (let iter = 0; iter < rows; iter++) {
-      let temp = [];
-      for (let iter2 = 0; iter2 < cols; iter2++) {
-        temp.push(0);
-      }
-      grid.push(temp);
-    }
+	componentDidUpdate(prevProps) {
+		if (this.props.fps !== prevProps.fps) {
+			this.timeInterval = 1000 / this.props.fps;
+		}
+	}
 
-    if (presetProvided) {
-      preset.forEach(loc => {
-        grid[loc.x][loc.y] = 1;
-      });
-    }
+	_getNewGrid = (preset = []) => {
+		const presetProvided = preset.length > 0;
+		const { rows, cols } = this.props;
+		let grid = [];
+		for (let iter = 0; iter < rows; iter++) {
+			let temp = [];
+			for (let iter2 = 0; iter2 < cols; iter2++) {
+				temp.push(0);
+			}
+			grid.push(temp);
+		}
 
-    return grid;
-  };
+		if (presetProvided) {
+			preset.forEach(loc => {
+				grid[loc.x][loc.y] = 1;
+			});
+		}
 
-  _getAliveCells() {
-    const currentGrid = this.state.grid;
-    return currentGrid.reduce(
-      (acc, val) => (acc += val.reduce((acc2, val2) => (acc2 += val2), 0)),
-      0
-    );
-  }
+		return grid;
+	};
 
-  onClickCell = (e, pos) => {
-    e.preventDefault();
-    const [x, y] = pos;
-    let currentGrid = this.state.grid;
-    if (currentGrid[x][y] === 1) {
-      currentGrid[x][y] = 0;
-    } else if (currentGrid[x][y] === 0) {
-      currentGrid[x][y] = 1;
-    }
+	_getAliveCells() {
+		const currentGrid = this.state.grid;
+		return currentGrid.reduce(
+			(acc, val) =>
+				(acc += val.reduce((acc2, val2) => (acc2 += val2), 0)),
+			0,
+		);
+	}
 
-    this.setState({ grid: currentGrid });
-  };
+	onClickCell = (e, pos) => {
+		e.preventDefault();
+		const [x, y] = pos;
+		let currentGrid = this.state.grid;
+		if (currentGrid[x][y] === 1) {
+			currentGrid[x][y] = 0;
+		} else if (currentGrid[x][y] === 0) {
+			currentGrid[x][y] = 1;
+		}
 
-  _getRandomCellState() {
-    return Math.floor(Math.random() * Math.floor(2));
-  }
+		this.setState({ grid: currentGrid });
+	};
 
-  _getNeighbourCount = (row, col) => {
-    const directions = [
-      { x: 0, y: 1 },
-      { x: 0, y: -1 },
-      { x: 1, y: 0 },
-      { x: -1, y: 0 },
-      { x: 1, y: 1 },
-      { x: 1, y: -1 },
-      { x: -1, y: -1 },
-      { x: -1, y: 1 }
-    ];
-    const { rows, cols } = this.props;
-    let neighbourCount = 0;
+	_getRandomCellState() {
+		return Math.floor(Math.random() * Math.floor(2));
+	}
 
-    directions.forEach(dir => {
-      const nei_row = row + dir.x;
-      const nei_col = col + dir.y;
-      if (nei_row >= 0 && nei_col >= 0 && nei_row < rows && nei_col < cols) {
-        if (this.state.grid[nei_row][nei_col]) {
-          neighbourCount++;
-        }
-      }
-    });
+	_getNeighbourCount = (row, col) => {
+		const directions = [
+			{ x: 0, y: 1 },
+			{ x: 0, y: -1 },
+			{ x: 1, y: 0 },
+			{ x: -1, y: 0 },
+			{ x: 1, y: 1 },
+			{ x: 1, y: -1 },
+			{ x: -1, y: -1 },
+			{ x: -1, y: 1 },
+		];
+		const { rows, cols } = this.props;
+		let neighbourCount = 0;
 
-    return neighbourCount;
-  };
+		directions.forEach(dir => {
+			const nei_row = row + dir.x;
+			const nei_col = col + dir.y;
+			if (
+				nei_row >= 0 &&
+				nei_col >= 0 &&
+				nei_row < rows &&
+				nei_col < cols
+			) {
+				if (this.state.grid[nei_row][nei_col]) {
+					neighbourCount++;
+				}
+			}
+		});
 
-  _gotoNextState = () => {
-    const { grid } = this.state;
-    const { rows, cols } = this.props;
-    let nextGrid = this._getNewGrid();
+		return neighbourCount;
+	};
 
-    for (let r = 0; r < rows; r++) {
-      for (let c = 0; c < cols; c++) {
-        const neighbourCount = this._getNeighbourCount(r, c);
-        if (neighbourCount === 3 && grid[r][c] === 0) {
-          nextGrid[r][c] = 1;
-        }
-        if (
-          (neighbourCount === 2 || neighbourCount === 3) &&
-          grid[r][c] === 1
-        ) {
-          nextGrid[r][c] = 1;
-        }
-      }
-    }
+	_gotoNextState = () => {
+		const { grid } = this.state;
+		const { rows, cols } = this.props;
+		let nextGrid = this._getNewGrid();
 
-    return nextGrid;
-  };
+		for (let r = 0; r < rows; r++) {
+			for (let c = 0; c < cols; c++) {
+				const neighbourCount = this._getNeighbourCount(r, c);
+				if (neighbourCount === 3 && grid[r][c] === 0) {
+					nextGrid[r][c] = 1;
+				}
+				if (
+					(neighbourCount === 2 || neighbourCount === 3) &&
+					grid[r][c] === 1
+				) {
+					nextGrid[r][c] = 1;
+				}
+			}
+		}
 
-  onClickNextState = () => {
-    const grid = this._gotoNextState();
-    this.setState({ grid });
-  };
+		return nextGrid;
+	};
 
-  _nextBoardState = () => {
-    const nextGrid = this._gotoNextState();
+	onClickNextState = () => {
+		const grid = this._gotoNextState();
+		this.setState({ grid });
+	};
 
-    this.setState(
-      {
-        grid: nextGrid,
-        globalAnimationID: window.requestAnimationFrame(this._nextBoardState)
-      },
-      () => {
-        if (this._getAliveCells() === 0) {
-          this._stopAnimation();
-        }
-      }
-    );
-  };
+	_nextBoardState = timestamp => {
+		this.setState(
+			{
+				globalAnimationID: window.requestAnimationFrame(
+					this._nextBoardState,
+				),
+			},
+			() => {
+				let timeElapsed = timestamp - this.previousFrame;
+				if (timeElapsed > this.timeInterval) {
+					const grid = this._gotoNextState();
+					this.previousFrame =
+						timestamp - (timeElapsed % this.timeInterval);
+					this.setState({ grid });
+				}
 
-  tickBoard = e => {
-    e.preventDefault();
+				if (this._getAliveCells() === 0) {
+					this._stopAnimation();
+				}
+			},
+		);
+	};
 
-    if (this._getAliveCells() === 0) {
-      alert("The Board is Empty!");
-      return;
-    }
-    this.setState({
-      globalAnimationID: window.requestAnimationFrame(this._nextBoardState),
-      isAnimating: true
-    });
-  };
+	tickBoard = e => {
+		e.preventDefault();
 
-  _stopAnimation = () => {
-    this.setState({ isAnimating: false });
-    window.cancelAnimationFrame(this.state.globalAnimationID);
-  };
+		if (this._getAliveCells() === 0) {
+			alert("The Board is Empty!");
+			return;
+		}
 
-  pauseBoard = e => {
-    e.preventDefault();
-    this._stopAnimation();
-  };
+		this.previousFrame = window.performance.now();
+		this.setState({
+			globalAnimationID: window.requestAnimationFrame(
+				this._nextBoardState,
+			),
+			isAnimating: true,
+		});
+	};
 
-  clearBoard = e => {
-    e.preventDefault();
+	_stopAnimation = () => {
+		this.setState({ isAnimating: false });
+		window.cancelAnimationFrame(this.state.globalAnimationID);
+	};
 
-    this.setState({
-      grid: this._getNewGrid(),
-      isAnimating: false,
-      globalAnimationID: undefined
-    });
-  };
+	pauseBoard = e => {
+		e.preventDefault();
+		this._stopAnimation();
+	};
 
-  createRandomGrid = e => {
-    e.preventDefault();
-    let grid = this._getNewGrid();
-    const { rows, cols } = this.props;
+	clearBoard = e => {
+		e.preventDefault();
 
-    for (let iter = 0; iter < rows; iter++) {
-      for (let iter2 = 0; iter2 < cols; iter2++) {
-        grid[iter][iter2] = this._getRandomCellState();
-      }
-    }
+		this.setState({
+			grid: this._getNewGrid(),
+			isAnimating: false,
+			globalAnimationID: undefined,
+		});
+	};
 
-    this.setState({ grid });
-  };
+	createRandomGrid = e => {
+		e.preventDefault();
+		let grid = this._getNewGrid();
+		const { rows, cols } = this.props;
 
-  _renderGrid = () => {
-    return this.state.grid.map((cells, row) => {
-      return (
-        <div
-          className="cells-row"
-          key={`cell-row-${row}`}
-          style={{ width: `${this.props.cols * 15}` }}
-        >
-          {cells.map((cell, col) => (
-            <Cell
-              isAlive={cell === 1}
-              pos={[row, col]}
-              onClick={this.onClickCell}
-              key={`cell-${row}-${col}`}
-            />
-          ))}
-        </div>
-      );
-    });
-  };
+		for (let iter = 0; iter < rows; iter++) {
+			for (let iter2 = 0; iter2 < cols; iter2++) {
+				grid[iter][iter2] = this._getRandomCellState();
+			}
+		}
 
-  loadGridFromPreset = (e, preset) => {
-    e.preventDefault();
-    const grid = this._getNewGrid(preset);
-    this.setState({ grid });
-  };
+		this.setState({ grid });
+	};
 
-  _renderPresetOptions = () => {
-    return STARTERS.map(starter => (
-      <Preset
-        presetName={starter.name}
-        key={starter.name}
-        onClick={this.loadGridFromPreset}
-      />
-    ));
-  };
+	_renderGrid = () => {
+		return this.state.grid.map((cells, row) => {
+			return (
+				<div
+					className="cells-row"
+					key={`cell-row-${row}`}
+					style={{ width: `${this.props.cols * 15}` }}
+				>
+					{cells.map((cell, col) => (
+						<Cell
+							isAlive={cell === 1}
+							pos={[row, col]}
+							onClick={this.onClickCell}
+							key={`cell-${row}-${col}`}
+						/>
+					))}
+				</div>
+			);
+		});
+	};
 
-  render() {
-    const { rows, cols } = this.props;
-    const { isAnimating } = this.state;
-    return (
-      <div className="game-container">
-        <div className="top">
-          <Stats numberAlive={this._getAliveCells()} />
-          <div className="preset-wrapper">{this._renderPresetOptions()}</div>
-        </div>
-        <div
-          className="cells-box"
-          style={{ height: `${12 * rows}px`, width: `${12 * cols}px` }}
-        >
-          {this._renderGrid()}
-        </div>
-        <Controls
-          onClickHandlers={[
-            this.tickBoard,
-            this.pauseBoard,
-            this.onClickNextState,
-            this.clearBoard,
-            this.createRandomGrid
-          ]}
-          isAnimating={isAnimating}
-        />
-      </div>
-    );
-  }
+	loadGridFromPreset = (e, preset) => {
+		e.preventDefault();
+		const grid = this._getNewGrid(preset);
+		this.setState({ grid });
+	};
+
+	_renderPresetOptions = () => {
+		return STARTERS.map(starter => (
+			<Preset
+				presetName={starter.name}
+				key={starter.name}
+				onClick={this.loadGridFromPreset}
+			/>
+		));
+	};
+
+	render() {
+		const { rows, cols } = this.props;
+		const { isAnimating } = this.state;
+		return (
+			<div className="game-container">
+				<div className="top">
+					<Stats numberAlive={this._getAliveCells()} />
+					<div className="preset-wrapper">
+						{this._renderPresetOptions()}
+					</div>
+				</div>
+				<div
+					className="cells-box"
+					style={{ height: `${12 * rows}px`, width: `${12 * cols}px` }}
+				>
+					{this._renderGrid()}
+				</div>
+				<Controls
+					onClickHandlers={[
+						this.tickBoard,
+						this.pauseBoard,
+						this.onClickNextState,
+						this.clearBoard,
+						this.createRandomGrid,
+					]}
+					isAnimating={isAnimating}
+				/>
+			</div>
+		);
+	}
 }
 
 export default Conway;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState, useEffect, memo } from "react";
 import ReactDOM from "react-dom";
 
 import Conway from "./components/Conway";
 import "./styles/styles.css";
 
-const Index = () => {
-  return (
-    <div className="container">
-      <Conway rows={35} cols={85} fps={60} />
-    </div>
-  );
+const changeFPS = prevVal => {
+	const changeFPSTo = prompt("Change FPS to?");
+	if (changeFPSTo === null) {
+		return prevVal;
+	}
+	return parseInt(changeFPSTo, 10);
 };
+
+const Index = memo(() => {
+	const [fps, updateFPS] = useState(10);
+
+	useEffect(() => {
+		document.title = `Game of Life @ ${fps} FPS`;
+	}, [fps]);
+
+	return (
+		<div
+			className="container"
+			onDoubleClick={() => {
+				const newfps = changeFPS(fps);
+				updateFPS(newfps);
+			}}
+		>
+			<Conway rows={35} cols={85} fps={fps} />
+		</div>
+	);
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Index />, rootElement);
